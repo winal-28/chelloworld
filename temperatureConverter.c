ecb205e70ba05cb4bd974d11d50a5a94afa89ea1
alwin <winalrollevi@gmail.com>
2021-07-28 17:03:03 +0800

    made the table print itself out in reverse order by modifying the for loop

--- temperatureConverter.c
+++ temperatureConverter.c
@@ -6,7 +6,7 @@ int main()
 
     printf("\nCelsius\t\tFahrenheit\n\n"); //labels
 
-    for (fahr = 0; fahr <= 100; fahr++)
+    for (fahr = 300; fahr >= 0; fahr--)
     {
         printf("%3.0f\t\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32)); // causes the values of the two integers fahr and celsius to be printed, with a tab (\t) between them
     }
