92ad1c35b45a7d267b56396fede5d9cea54c6766
alwin <winalrollevi@gmail.com>
2021-07-28 17:05:50 +0800

    Symbolic Constants

--- temperatureConverter.c
+++ temperatureConverter.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define MIN 0
+#define MAX 300
+#define STEP 1
+
 int main()
 {
     float fahr;
 
     printf("\nCelsius\t\tFahrenheit\n\n"); //labels
 
-    for (fahr = 300; fahr >= 0; fahr--)
+    for (fahr = MAX; fahr >= MIN; fahr = fahr - STEP)
     {
         printf("%3.0f\t\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32)); // causes the values of the two integers fahr and celsius to be printed, with a tab (\t) between them
     }
