b2f765957188b8b1e8601f47ca9e139a1e943c42
alwin <winalrollevi@gmail.com>
2021-07-28 13:21:13 +0800

    rewrote the program where it uses a for loop instead of a while loop, making it more cleaner

--- temperatureConverter.c
+++ temperatureConverter.c
@@ -2,25 +2,13 @@
 
 int main()
 {
-    float fahr, celsius;
-    float min, max, step;
+    float fahr;
 
-    min = 0;   // minimum of the temperature scale is 0;
-    max = 300; // maximum of the temperature scale is 300;
-    step = 20; // skips by 20 each count, step size, idk how you actually call it lmao;
+    printf("\nCelsius\t\tFahrenheit\n\n"); //labels
 
-    fahr = min; // at first, fahrenheit's value is 0
-
-    printf("Celsius\tFahrenheit\n"); //labels
-
-    for (fahr = min; fahr <= max; fahr++)
+    for (fahr = 0; fahr <= 100; fahr++)
     {
-        /* while the value of fahrenheit is less than or equal to the maximum (which is set to 300),
-         it's going to keep converting the fahrenheit value to celcius and also skip by 20
-        */
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius); // causes the values of the two integers fahr and celsius to be printed, with a tab (\t) between them
-        fahr = fahr + step;
+        printf("%3.0f\t\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32)); // causes the values of the two integers fahr and celsius to be printed, with a tab (\t) between them
     }
 
     return 0;
